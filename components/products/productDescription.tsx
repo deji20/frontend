d138a80e4e692e73569d4bcc25f29341f12416ec
Deji <ehinlanwo.deji@live.dk>
2022-02-02 12:15:40 +0100

    init commit

--- components/products/productDescription.tsx
+++ components/products/productDescription.tsx
@@ -6,13 +6,14 @@ import ProductPrice from "./productPrice";
 import { ProductModel } from "../../models/models";
 
 export default function ProductDescription(props: {product: ProductModel}){
+    let version = props.product.version[0];
     return (
             <div className="h-full flex flex-col bg-purple-900 bg-opacity-20 overflow-ellipsis">
                 <div className="px-10 border-b border-gray-700 text-xl text-white">
                     <h3>{props.product.name}</h3>
                 </div>
                 <div className="overflow-hidden flex flex-1 place-items-center">
-                        <p className="text-sm px-10 text-white overflow-ellipsis">{props.product.description}</p>
+                        <p className="text-sm px-10 text-white overflow-ellipsis">{version.description}</p>
                 </div>
                 <div className="flex justify-end">
                     <div className="bg-white bg-opacity-10 rounded-tl-lg px-2">
