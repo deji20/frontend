de6ad7560b75bd37d863ed1372a411e6e12aff39
deji <ehinlanwo.deji@live.dk>
2022-02-17 10:02:20 +0100

    init commit

--- components/products/productHero.tsx
+++ components/products/productHero.tsx
@@ -13,7 +13,7 @@ export default function ProductHero(props: ProductHeroProps){
     if(!error && !categories) return <p>Loading</p>
     return (
         <div className="flex flex-row w-full justify-evenly p-10 pt-20 bg-gray-800">
-            {categories && categories.map((category, i) => <FrontCategories key={i} className='h-[15rem] w-20 sm:h-[25rem] md:h-[35rem] sm:w-40 md:w-52 lg:w-80' name={category} href={`/${category}`}/>) }} 
+            {categories && categories.map((category, i) => <FrontCategories key={i} className='h-[15rem] w-20 sm:h-[25rem] md:h-[35rem] sm:w-40 md:w-52 lg:w-80' name={category} href={`/${category}`}/>) } 
         </div>
     )
 }
