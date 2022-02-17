ab877bf9827e68a12274ebb80a93add5aa827183
Viktor Ehinlanwo <ehinlanwo.deji@live.dk>
2022-02-17 14:34:01 +0100

    one more time

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ class config{
 
     constructor(){
         this.api = process.env.API || process.env.NEXT_PUBLIC_API || "";
-        this.port = parseInt(process.env.PORT) || 8080;
+        this.port = parseInt(process.env.PORT || "8080");
     }
 }
 
