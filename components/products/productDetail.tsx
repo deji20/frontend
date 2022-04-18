11b2bbc08b711c0584f6d73f663638ad763f8cb2
deji <ehinlanwo.deji@live.dk>
2022-04-18 11:57:20 +0200

    debugging undefined product details page null check

--- components/products/productDetail.tsx
+++ components/products/productDetail.tsx
@@ -10,14 +10,13 @@ import Loading from "../fallback/loading";
 export default function ProductDetail(props: {product: ProductModel}){
     const [dialog, dialogOpen] = useState(false)
     const [dialogImage, changeImage] = useState(0)
-    console.log("product", !props.product?.version?.[0])
     if(!props.product?.version?.[0]) return <Loading/>
-    let version = props.product.version[0]
+    let version = props.product.version?.[0]
 
     const imageDialog = (
         <Dialog onClose={() => dialogOpen(false)} darken={true}>
             <div className="h-full w-full relative">
-                <Img imgClass="rounded" className="shadow-xl rounded" pictures={version.pictures} />
+                <Img imgClass="rounded" className="shadow-xl rounded" pictures={version?.pictures} />
             </div>
         </Dialog>
         )
@@ -29,13 +28,13 @@ export default function ProductDetail(props: {product: ProductModel}){
                 <div className="grid grid-rows-2 h-full w-80">
                     <div className="w-full p-10 cursor-pointer hover:p-1 hover:scale-105 transition-transform" onClick={() => dialogOpen(true)}>
                         <div className="relative h-full w-full hover:p-10">
-                            <Img className="rounded" pictures={version.pictures} />
+                            <Img className="rounded" pictures={version?.pictures} />
                         </div>
                     </div>
                     <div className="flex flex-grow align-middle justify-center">
                         <div className="m-3 p-2 h-20 place-self-center bg-gray-800 bg-opacity-20 rounded-xl shadow-xl">
                             <p className="font-light text-white">
-                                {version.description}
+                                {version?.description}
                             </p>
                         </div>
                     </div>
