fc3d4811b96d894b5a7a99d2e253d3dfffed86ae
deji <ehinlanwo.deji@live.dk>
2022-06-19 20:38:09 +0200

    fixed cart

--- components/cart/cart.tsx
+++ components/cart/cart.tsx
@@ -9,7 +9,7 @@ export default function Cart(){
     return (
         <Link href="/cart"  passHref>
             <div className="z-50 fixed rounded-full bottom-2 right-2 flex items-center opacity-50 cursor-pointer transform duration-500 hover:scale-105 hover:opacity-100">
-                <div className="absolute z-30 bottom-2 right-2 text-center bg-white bg-opacitity-50 text-xs w-5 h-5 rounded-full">{cart?.products?.length}</div>}
+                <div className="absolute z-30 bottom-2 right-2 text-center bg-white bg-opacitity-50 text-xs w-5 h-5 rounded-full">{cart?.products?.length}</div>
                 <div className="flex w-20 h-20">
                     <Image className="" pictures={["/assets/cart.png"]} />
                 </div>
